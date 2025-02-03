e3d031d141e03e58b9b4bb659f82f5f57b69aae4
RoCry <crysheen@gmail.com>
2025-02-03 16:19:06 +0800

    feat: 📋 add initial rss feed configuration and readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# FeedForger 🔨⚡
+_Formerly from [RoCry/feedcooker](https://github.com/RoCry/feedcooker)_
+
+[![GitHub Actions Status](https://img.shields.io/github/actions/workflow/status/RoCry/feedforger/main.yml?style=flat-square)](https://github.com/RoCry/feedforger/actions)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Automatically forge multiple RSS feeds into organized JSON groups using GitHub Actions.
+
---
+++ recipes.py
@@ -0,0 +1,40 @@
+_recipes = {
+    "GithuberFeeds": {
+        "urls": [
+            "https://github.com/norsez.atom",
+            "https://github.com/nst.atom",
+            "https://github.com/omz.atom",
+            "https://github.com/peng-zhihui.atom",
+            "https://github.com/qhduan.atom",
+            "https://github.com/rentzsch.atom",
+            "https://github.com/rnapier.atom",
+            "https://github.com/robb.atom",
+            "https://github.com/samvermette.atom",
+            "https://github.com/stefanoa.atom",
+            "https://github.com/theowenyoung.atom",
+            "https://github.com/ishan0102.atom",
+        ],
+        "filters": [
+            {
+                "title": "commented on|closed an issue|opened an issue|merged a pull request|pushed to|deleted branch|created a tag|created a branch",
+                "invert": True,
+            },
+        ],
+    },
+    "Rust News": {
+        "urls": [
+            "https://hnrss.org/frontpage.atom?q=rust",
+            "https://reddit.com/rust.rss",
+        ]
+    },
+}
+
+
+def get_recipes() -> dict[str, dict]:
+    return _recipes
+
+
+if __name__ == "__main__":
+    import json
+
+    print(json.dumps(get_recipes()))
